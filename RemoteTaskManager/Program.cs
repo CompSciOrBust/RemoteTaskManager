81b05363f34ddc81a7881d3111d9939af0fe05bb
CompSciOrBust <54033033+CompSciOrBust@users.noreply.github.com>
2019-08-13 23:35:00 +0100

    Added task killer and updated html
    Added a page for killing processes and added html for 404 & for when the user powers down / reboots.

--- RemoteTaskManager/Program.cs
+++ RemoteTaskManager/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
@@ -10,6 +11,7 @@ namespace RemoteTaskManager
 {
     class Program
     {
+        //Default web port
         static int Port = 80;
 
         static void Main(string[] args)
@@ -34,20 +36,76 @@ namespace RemoteTaskManager
                         if(Tokens[1] == "/")//Default page requested
                         {
                             ClientWriter.WriteLine("HTTP/1.0 520 OK\n"); //Response Header
-                            ClientWriter.WriteLine("<html><center><h2><a href=\"/Reboot\">Reboot pc</a><br><a href=\"/Shutdown\">shutdown pc</a></h2></center></html>"); //Payload
+                            ClientWriter.WriteLine("<html><center><h2><a href=\"/Reboot\">Reboot pc</a><br><a href=\"/Shutdown\">Shutdown pc</a><br><a href=\"/TaskKill\">Task list</a></h2></center></html>"); //Payload
                             ClientWriter.Flush(); //Send data down the pipe
                         }
-                        if(Tokens[1] == "/Reboot")//Requesting a reboot
+                        else if(Tokens[1] == "/Reboot")//Requesting a reboot
                         {
                             ClientWriter.WriteLine("HTTP/1.0 520 OK\n"); //Response Header
+                            ClientWriter.WriteLine("<html><center><h2><p>Restarting.</p></h2></center></html>"); //Payload
+                            ClientWriter.Flush(); //Send data down the pipe
                             //Spawn shutdown.exe process with the arguments to reboot immediately
-                            System.Diagnostics.Process.Start("shutdown.exe", "-r -t 0");
+                            System.Diagnostics.Process.Start("shutdown.exe", "-r -t 0 -f");
                         }
-                        if(Tokens[2] == "/Shutdown")//Requesting a power off
+                        else if(Tokens[1] == "/Shutdown")//Requesting a power off
                         {
                             ClientWriter.WriteLine("HTTP/1.0 520 OK\n"); //Response Header
+                            ClientWriter.WriteLine("<html><center><h2><p>Powering down.</p></h2></center></html>"); //Payload
+                            ClientWriter.Flush(); //Send data down the pipe
                             //Spawn shutdown.exe process with the arguments to shutdown immediately
-                            System.Diagnostics.Process.Start("shutdown.exe", "-s -t 0");
+                            System.Diagnostics.Process.Start("shutdown.exe", "-s -t 0 -f");
+                        }
+                        else if(Tokens[1].Split('/')[1] == "TaskKill")// Kill Processes
+                        {
+                            Process[] ProcArray = Process.GetProcesses();//get list of processes
+                            ClientWriter.WriteLine("HTTP/1.0 520 OK\n");//Response Header
+                            if (Tokens[1].Split('/').Length == 2 || Tokens[1].EndsWith("/"))//Return a list of procs we can kill
+                            {
+
+                                ClientWriter.WriteLine("<html><center><h2><p>Task list:</p></h2></center></html>"); //Payload if task is killed
+                                foreach (Process Proc in ProcArray) //Add each task as a link to the payload
+                                {
+                                    ClientWriter.WriteLine("<center><h3><p><a href=\"/TaskKill/{0}\">" + Proc.ProcessName + "</a></p></h3></center>", Proc.Id); //Build payload
+                                }
+                            }
+                            else//Kill the process
+                            {
+                                int PIDToKill = int.Parse(Tokens[1].Split('/')[2]);//Get the PID from the url
+                                Process ProcToKill = null; //Var to store the process
+                                //Check if process exists
+                                foreach (Process Proc in ProcArray)
+                                {
+                                    //If the process ID matches assign the process to ProcToKill
+                                    if(Proc.Id == PIDToKill)
+                                    {
+                                        ProcToKill = Proc;
+                                    }
+                                }
+                                if(ProcToKill != null)//Check process exists
+                                {
+                                    ProcToKill.Kill();//Kill the process
+                                    if (ProcToKill.HasExited)
+                                    {
+                                        ClientWriter.WriteLine("<html><center><h2><p>Task killed.</p><br><a href=\"/\">Home</a></h2></center></html>"); //Payload if task is killed
+                                    }
+                                    else
+                                    {
+                                        ClientWriter.WriteLine("<html><center><h2><p>Failed to kill task.</p><br><a href=\"/\">Home</a></h2></center></html>"); //Payload if task is failed.
+                                    }
+                                }
+                                else
+                                {
+                                    ClientWriter.WriteLine("<html><center><h2><p>Could not find task.</p><br><a href=\"/\">Home</a></h2></center></html>"); //Payload if task is failed.
+                                }
+
+                            }
+                            ClientWriter.Flush(); //Send data down the pipe
+                        }
+                        else //Unknown request
+                        {
+                            ClientWriter.WriteLine("HTTP/1.0 404 OK\n"); //Response Header. 404 is not ok but we'll lie because it doesn't make a difference.
+                            ClientWriter.WriteLine("<html><center><h2></p>404. Unknown request.</p><br><a href=\"/\">Home</a></h2></center></html>"); //Payload.
+                            ClientWriter.Flush(); //Send data down the pipe
                         }
                         break;
                     default:
